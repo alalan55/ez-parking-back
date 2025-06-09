5e20a99262e48cde775d188380d144b883d31ea8
alalan55 <alla.ao4975@gmail.com>
2025-06-09 17:33:16 -0300

    Insere instalação de swagger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import database from "./config/db.js";
+import { swaggerSpec, swaggerUi } from "./config/swagger.js";
 
 import "./models/index.js";
 
@@ -15,7 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/vehicle", vehicleRouter);
 app.use("/client", clientRouter);
 
-app.get("/", (res) => res.send("Health"));
+app.get("/", (req, res) => res.send("Health"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 (async () => {
   try {
