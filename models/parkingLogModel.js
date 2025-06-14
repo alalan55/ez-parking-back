48ac88f34aaa4e987c0f57d215b1078849e58076
alalan55 <alla.ao4975@gmail.com>
2025-06-14 13:38:49 -0300

    remove relação para deixar apenas na tabela alvo

--- models/parkingLogModel.js
+++ models/parkingLogModel.js
@@ -8,22 +8,18 @@ import Vehicle from "./vehicleModel.js";
 const ParkingLogModel = sequelize.define("ParkingLog", {});
 
 // vacancy
-ParkingLogModel.hasOne(VacancyModel);
 ParkingLogModel.belongsTo(VacancyModel);
 VacancyModel.hasMany(ParkingLogModel);
 
 // organization
-ParkingLogModel.hasOne(OrganizationModel);
 ParkingLogModel.belongsTo(OrganizationModel);
 OrganizationModel.hasMany(ParkingLogModel);
 
 // collaborator
-ParkingLogModel.hasOne(CollaboratorModel);
 ParkingLogModel.belongsTo(CollaboratorModel);
 CollaboratorModel.hasMany(ParkingLogModel);
 
 // vehicle
-ParkingLogModel.hasOne(Vehicle);
 ParkingLogModel.belongsTo(Vehicle);
 Vehicle.hasMany(ParkingLogModel);
 
